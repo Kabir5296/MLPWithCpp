69bb409d9b82083add49bf8901848c9289b6c0c5
kabir5296 <manb5296@gmail.com>
2024-09-29 16:16:05 +0600

    ndarray methods in building

--- src/ndarray.cpp
+++ src/ndarray.cpp
@@ -5,11 +5,29 @@
 #include <random>
 #include <optional>
 
+/*
+n-Dimensional array implementation in C++.
+How it works:
+    Uses shapes and strides for n-Dim array, the actual array is saved in an 1D array in the memory.
+Methods:
+    calculateSize: calculates size of the 1D representation of an n-D array.
+    calculateStrides: Given a shape, calculates the strides array.
+    calculateIndex: Converts a given index in the n-Dim array to position in the 1D representation.
+    indexToIndices: Does the exact reverse of the previous.
+    T: Transpose function to be applied on current data. **Currently only implemented full transpose.**
+    transpose: Transpose function to be used on external data. **Currently only implemented full transpose.**
+    reshape: Reshaping the n-Dim array.
+    zeroInit: Initializes the array with all 0 values.
+    randomInit: Initializes the array with random values.
+    operator[]: Uses [] operator to access array elements. i.e., arr[{i,j,k}] for a 3D array.
+*/
+
+template<typename Tem>
 class NDArray {
 private:
     std::vector<size_t> shape;
     std::vector<size_t> strides;
-    std::vector<double> data;
+    std::vector<Tem> data;
     size_t num_dim;
 
     size_t calculateSize(std::vector<size_t> shape) const {
@@ -78,41 +96,78 @@ public:
         strides = calculateStrides(num_dim, new_shape);
     }
 
-    void transpose() {
+    bool operator==(const NDArray& other) const {
+        if (data.size() != other.data.size() || shape != other.shape)
+            return false;
+        return std::equal(data.begin(), data.end(), other.data.begin());
+    }
+
+    bool operator!=(const NDArray& other) const {
+        return !(*this == other);
+    }
+
+    void T() {
         std::vector<size_t> oldShape = shape;
         std::vector<size_t> oldStrides = strides;
 
         std::reverse(shape.begin(), shape.end());
         strides = calculateStrides(num_dim, shape);
         
-        std::vector<double> transpose_data(data.size());
+        std::vector<Tem> transposeData(data.size());
+        std::vector<size_t> oldIndices(num_dim,0);
 
         for (size_t i=0; i<data.size(); ++i) {
-            std::vector <size_t> oldIndices = indexToIndices(i);
+            oldIndices = indexToIndices(i);
             std::reverse(oldIndices.begin(), oldIndices.end());
             size_t newIndex = calculateIndex(oldIndices);
-            transpose_data[newIndex] = data[i];            
+            transposeData[newIndex] = data[i];            
         }
-        data = std::move(transpose_data);
+        data = std::move(transposeData);
+    }
+
+    NDArray transpose(NDArray arr) {
+        std::vector<size_t> oldShape = arr.shape;
+        std::vector<size_t> oldStrides = arr.strides;
+
+        std::reverse(arr.shape.begin(), arr.shape.end());
+        arr.strides = calculateStrides(arr.num_dim, arr.shape);
+        
+        std::vector<Tem> transposeData(arr.data.size());
+        std::vector<size_t> oldIndices(num_dim,0);
+
+        for (size_t i=0; i<arr.data.size(); ++i) {
+            oldIndices = indexToIndices(i);
+            std::reverse(oldIndices.begin(), oldIndices.end());
+            size_t newIndex = calculateIndex(oldIndices);
+            transposeData[newIndex] = arr.data[i];            
+        }
+        arr.data = std::move(transposeData);
+        return arr;
     }
 
     void zeroInit() {
-        std::fill(data.begin(), data.end(), 0.0);
+        std::fill(data.begin(), data.end(), Tem(0.0));
     }
 
-    void randomInit(double min=-0.1, double max=0.1, std::optional<unsigned> seed = std::nullopt) {
+    void randomInit(Tem min = Tem(-0.1), Tem max = Tem(0.1), std::optional<unsigned> seed = std::nullopt) {
         std::default_random_engine gen;
         if (seed) 
             gen.seed(*seed);
         else {           
             std::random_device rd;
             gen.seed(rd());
+        } 
+        if constexpr (std::is_integral_v<T>) {
+            std::uniform_int_distribution<T> dis(min, max);
+            std::generate(data.begin(), data.end(), [&]() {return dis(gen);});
         }
-        std::uniform_real_distribution<double> dis(min, max);
-        std::generate(data.begin(), data.end(), [&]() {return dis(gen);});
+        else {
+            std::uniform_real_distribution<T> dis(min, max);
+            std::generate(data.begin(), data.end(), [&]() {return dis(gen);});
+        }
     }
 
-    double operator[](std::vector<size_t> indices) {
+    Tem operator[](std::vector<size_t> indices) {
         return data[calculateIndex(indices)];
     }    
 
